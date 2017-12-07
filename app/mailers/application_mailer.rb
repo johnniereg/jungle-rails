dd30270f009ffb6dc8ed71919a52b1aa622a5cb7
Johnnie Regalado <johnniereg@gmail.com>
2017-12-07 23:49:56 +0000

    Set-up email receipts.
    Created action mailer, started template, testing sends.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@jungle.com"
   layout 'mailer'
+
+  ActionMailer::Base.delivery_method = :test
+
+
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
 
-  def order_receipt_email(order_details)
+  def order_receipt_email(order)
+    @order = order
+    mail(to: @order.email, subject: "Your Jungle Order ID #{@order.id}")
+
   end
 
 
