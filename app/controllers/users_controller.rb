62db46f039bd147ae599525ebc10bdc188b1cb74
Johnnie Regalado <johnniereg@gmail.com>
2017-12-08 02:27:44 +0000

    Added basic flash error messages.
    Invalid logins or registrations display flash message errors.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:error] = user.errors.full_messages.to_sentence
       redirect_to '/users/new'
     end
   end
