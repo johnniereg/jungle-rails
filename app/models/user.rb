e1f43081b73b704418b98f38e32ae4d28181e0c0
Johnnie Regalado <johnniereg@gmail.com>
2017-12-08 00:58:38 +0000

    Added user email unique check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     has_secure_password
-    validates :first_name, :last_name, :email, presence: true
+    validates :first_name, :last_name, presence: true
+    validates :email, uniqueness: true, presence: true
 end
