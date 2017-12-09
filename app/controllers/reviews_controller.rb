d673ac0a823bffb3ef68b0810e6883e17657ec22
Johnnie Regalado <johnniereg@gmail.com>
2017-12-09 00:57:47 +0000

    Allow deletion of reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-
+  before_action :require_login, only: [:create, :destroy]
 
   def create
 
@@ -8,7 +8,6 @@ class ReviewsController < ApplicationController
     @review.product = @product
     @review.user = current_user
     
-    
     if @review.save!
       flash[:notice] = 'Review successfully submitted.'
       redirect_to @product
@@ -16,11 +15,25 @@ class ReviewsController < ApplicationController
       render @product
     end
     
+  end
 
-
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to @review.product
   end
 
   private
+
+    def logged_in?
+    end
+
+    def require_login
+      unless current_user
+        flash[:error] = "You must be logged in to access this section."
+        redirect_to @product
+      end
+    end
 
     def review_params
       params.require(:review).permit(:rating, :description)
