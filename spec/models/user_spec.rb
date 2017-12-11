8c51c8d3e839fec583d1f914374baa07799c325b
Johnnie Regalado <johnniereg@gmail.com>
2017-12-11 20:58:31 +0000

    Test for nil passwords.
    Checking validation of passwords.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    context 'on a new user' do
+
+      it 'should not be valid without a password' do
+        @user = User.new password: nil, password_confirmation: nil, first_name: 'Jo', last_name: 'Blo', email: 'test@test.com'
+        expect(@user).to_not be_valid
+      end
+
+    end
+
+
+
+  end
 end
