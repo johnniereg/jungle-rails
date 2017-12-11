7b4fd598edaafcc83f74fdf74e29c6a5632868f4
Johnnie Regalado <johnniereg@gmail.com>
2017-12-11 20:25:59 +0000

    Test for name validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,10 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    it 'returns true if all validations pass' do
+
+    it 'pass if all fields are filled' do
       @category = Category.create!
-      @product = Product.create!(name: 'Product', price: 1000, quantity: 5, category: @category)
-      expect(@product.valid?).to be(true)
+      @product = Product.new(name: 'Product', price: 1000, quantity: 5, category: @category)
+      expect(@product).to be_valid
     end
+
+    it 'fail if name is not filled' do
+      @category = Category.create!
+      @product = Product.new(name: nil, price: 1000, quantity: 5, category: @category)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+
   end
 end
