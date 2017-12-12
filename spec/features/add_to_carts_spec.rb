8e4e6237c8adf02d9571d5fe6e07f70a414ac3fa
Johnnie Regalado <johnniereg@gmail.com>
2017-12-12 20:56:57 +0000

    Wrote test for add to cart feature.
    Verifies clicks on 'Add' increase nav cart amount.

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "AddToCarts", type: :feature, js: true do
+
+  #SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can click Add To Cart and increase cart count" do
+    # ACT
+    visit root_path
+
+    click_link('Add', match: :first)
+
+    sleep 5
+
+    # DEBUG / VERIFY
+    save_screenshot
+
+    expect(page).to have_text 'My Cart (1)'
+
+
+  end
+  
 end
