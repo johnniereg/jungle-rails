fc18e342fb5e55a050416dcb6f5058a1cda2a61e
Johnnie Regalado <johnniereg@gmail.com>
2017-12-12 01:39:02 +0000

    Downcase emails on create and login check.
    Prevents input errors stopping user authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,14 @@ class User < ActiveRecord::Base
     validates :first_name, :last_name, presence: true
     validates :email, uniqueness: { case_sensitive: false }, presence: true
 
+    before_save :downcase_email
+
     def self.authenticate_with_credentials(email, password)
-        user = User.find_by(email: email.strip).try(:authenticate, password)
+        user = User.find_by(email: email.strip.downcase).try(:authenticate, password)
+    end
+
+    def downcase_email
+        self.email.downcase!
     end
 
 end
